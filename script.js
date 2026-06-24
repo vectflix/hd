e5c1dd54fd3150185de04d14a36e33ea10e0f37b
vectflix <malcolmotieno69@gmail.com>
2026-06-24 18:25:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,35 @@
-const btn = document.getElementById("enter-btn");
+const enterBtn = document.getElementById("enter-btn");
 const welcome = document.getElementById("welcome-screen");
 const main = document.getElementById("main-content");
 const music = document.getElementById("music");
 
-btn.addEventListener("click", async () => {
-  try {
-    await music.play();
-  } catch (e) {
-    console.log("Music couldn't start.", e);
-  }
+enterBtn.addEventListener("click", () => {
+  music.play();
 
   welcome.style.display = "none";
   main.classList.remove("hidden");
 
-  startEffects();
+  createEffects();
 });
 
-function startEffects() {
-  const emojis = ["🌹","❤️","✨","🎈","💕","💜","🎉"];
+function createEffects() {
+  const emojis = ["❤️","🌹","✨","🎈","💕"];
 
   setInterval(() => {
     const item = document.createElement("div");
-    item.className = "item";
+    item.classList.add("heart");
+
     item.innerHTML =
       emojis[Math.floor(Math.random() * emojis.length)];
 
     item.style.left = Math.random() * 100 + "vw";
     item.style.fontSize =
-      (20 + Math.random() * 25) + "px";
+      Math.random() * 20 + 20 + "px";
 
     document.body.appendChild(item);
 
     setTimeout(() => {
       item.remove();
-    }, 10000);
-  }, 300);
+    }, 8000);
+  }, 350);
 }
